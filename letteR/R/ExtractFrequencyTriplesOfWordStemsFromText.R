37bbccef2463b0f59da103a33f45872f1c8d497a
Ludovic BOCKEN <lbocken@gmail.com>
2020-05-13 11:59:20 -0400

    Create ExtractFrequencyTriplesOfWordStemsFromText.R

---
+++ letteR/R/ExtractFrequencyTriplesOfWordStemsFromText.R
@@ -0,0 +1,13 @@
+text <- 'This article is about the domestic dog. For related species known as "dogs", see Canidae. For other uses, see Dog (disambiguation).'
+triples.df <- ExtractFrequencyTriplesOfWordStemsFromText(text)
+
+ExtractFrequencyTriplesOfWordStemsFromText <- function(text){
+  text <- tolower(text)
+  wordStems <- sort(unlist(ExtractWordStemsFromText(text)))
+  frequency.df <- as.data.frame(table(wordStems))
+  
+  return.df <- data.frame(S = paste0('wordStems:', frequency.df$wordStems), 
+                          P = paste0('hasFrequency:', frequency.df$Freq), 
+                          O = text)
+  return(return.df)
+}
